454a57296ee77174219e04321407527ba9dd4e17
Terence <68271470+tsamba120@users.noreply.github.com>
2021-07-19 01:14:57 -0400

    Clean up deletion

--- dags/config.py
+++
@@ -1,8 +0,0 @@
-# Spotify Credentials
-spotify_client_id = '8144be8952304b279edba66aff8ca782'
-spotify_client_secret = 'b45fe683ebf547109658210d3f259256'
-
-# Postgres Credentials
-dbname = 'Spotify Data'
-user='postgres'
-password='babsy1995'
