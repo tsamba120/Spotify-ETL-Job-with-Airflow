8d3efb5f4c76f649e061c6efc480b6e79f92ec36
Terence <68271470+tsamba120@users.noreply.github.com>
2021-08-04 01:46:26 -0400

    8/24/21 - S3 Branch: Updated spotify_dag file

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -1,10 +1,9 @@
 from datetime import timedelta
 from datetime import datetime as dt
-import datetime
 from airflow import DAG # DAG object
 from airflow.operators.python import PythonOperator # Python operator
 from airflow.utils.dates import days_ago
-from etl_spotify import spotify_etl_func, test_func
+from etl_spotify import extract_stage_data, transform_load_data, spotify_etl_func
 
 # CHANGE AIRFLOW DIRECTORIES ALWAYS: 
 #   https://stackoverflow.com/questions/52698704/how-to-change-the-dag-bag-folder-for-airflow-web-ui
@@ -29,6 +28,18 @@ dag = DAG(
     schedule_interval = '@daily'
 )
 
+TASK_1 = PythonOperator(
+    task_id='extract_stage_to_s3',
+    python_callable=extract_stage_data,
+    dag=dag
+)
+
+TASK_2 = PythonOperator(
+    task_id='transform_validate_load',
+    python_callable=transform_load_data,
+    dag=dag
+)
+
 execute_spotify_job = PythonOperator(
     task_id='spotify_etl_pgsql',
     python_callable=spotify_etl_func,
@@ -36,3 +47,4 @@ execute_spotify_job = PythonOperator(
 )
 
 execute_spotify_job
+# TASK_1 >> TASK_2
