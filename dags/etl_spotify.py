879215d7031aa7786e15f7361ee424a00b2b7756
Terence <68271470+tsamba120@users.noreply.github.com>
2021-08-04 01:45:55 -0400

    8/4/21 - Updated etl functions to implement S3

--- dags/etl_spotify.py
+++ dags/etl_spotify.py
@@ -12,7 +12,7 @@ import psycopg2
 import sqlalchemy
 from config import spotify_client_id, spotify_client_secret, dbname, db_password, S3_BUCKET, AWS_ACCESS_KEY_ID, SECRET_ACCESS_KEY
 
-
+# TODO: Delete original extract_data() funct
 def extract_data():
     '''
     Connects to Spotify API via spotipy and collects 50 most recent songs played
@@ -125,8 +125,13 @@ def extract_data():
     print('Data extraction completed\n--------')
     return song_plays, dim_songs, dim_artists, dim_albums
 
+# USE
 def extract_stage_data(ti):
-        
+    '''
+    Connects to Spotify API via spotipy and collects 50 most recent songs played
+    Compresses data to gzip file then uploads in AWS S3 data lake
+    '''
+
     spotify_redirect_url = 'http://localhost/'
 
     scope = 'user-read-recently-played'
@@ -172,6 +177,8 @@ def extract_stage_data(ti):
 
     return time_stamp
 
+
+# USE
 def transform_data(ti):
     load_timestamp = ti.xcom_pull(key='return_value', task_ids='s3_load')
 
@@ -272,10 +279,24 @@ def transform_data(ti):
         'album_url': album_urls
     }
 
+    song_plays_df = pd.DataFrame(song_plays, columns=song_plays.keys())
     
-    return song_plays, dim_songs, dim_artists, dim_albums
+    dim_songs_df = pd.DataFrame(dim_songs, columns=dim_songs.keys())
+    dim_songs_df.drop_duplicates(subset=['song_id'], inplace=True)
+
+    dim_artists_df = pd.DataFrame(dim_artists, columns=dim_artists.keys())
+    dim_artists_df.drop_duplicates(subset=['artist_id'], inplace=True)
+
+    dim_albums_df = pd.DataFrame(dim_albums, columns=dim_albums.keys())
+    dim_albums_df.drop_duplicates(subset=['album_id'], inplace=True)
+
+    print('Data transformation completed\n--------')
+    # print(dim_artists_df.head())
+    # print(dim_albums_df[['album_id', 'album_name', 'artist_id']].head())
+    return song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df
     
 
+# TODO: Delete original transform_data() function
 def transform_data(song_plays, dim_songs, dim_artists, dim_albums):
     '''
     Transforms passed data dictionaries into dataframes
@@ -299,7 +320,7 @@ def transform_data(song_plays, dim_songs, dim_artists, dim_albums):
     return song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df
 
 
-
+# USE
 def validate_data(song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df):
     '''
     Performs data validation prior to loading into database
@@ -339,7 +360,7 @@ def validate_data(song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df):
 
     return True
 
-
+# USE
 def load_data(song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df): # Add dataframe parameters!
     '''
     Loads dataframes into PostgreSQL database using psycopg2 driver
@@ -421,7 +442,16 @@ def load_data(song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df): # Add
     alc_conn.close()
     pg_conn.close()
 
+# TODO: Edit below function based on new S3 feature
+## Figure out what to do with "ti" argument!!!
+def transform_load_data(ti):
+    song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df = transform_data(ti)
+    if validate_data(song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df):
+        pass
+    load_data(song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df)
 
+
+####
 def spotify_etl_func():
     song_plays, dim_songs, dim_artists, dim_albums = extract_data()
     song_plays_df, dim_songs_df, dim_artists_df, dim_albums_df = transform_data(song_plays, dim_songs, dim_artists, dim_albums)
@@ -431,8 +461,8 @@ def spotify_etl_func():
     
     print(f"Daily Spotify ETL Job Completed - {dt.now().strftime('%m/%d/%Y - %H:%M:%S')}")
 
-def test_func():
-    print('Testing that this DAG is working')
+
 
 if __name__ == '__main__':
-    spotify_etl_func()
+    # spotify_etl_func()
+    pass
